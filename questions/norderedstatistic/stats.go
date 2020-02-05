1eddc032d578c4fe343ff8631e0287b8d8d694fe
Joshua <joshroppo@gmail.com>
2020-02-05 11:29:49 -0800

    Find the smallest N numbers via efficient sorting

--- questions/norderedstatistic/stats.go
+++ questions/norderedstatistic/stats.go
@@ -1,7 +1,10 @@
 // Package norderedstatistic wat
 package norderedstatistic
 
-import "math"
+import (
+	"math"
+	"sort"
+)
 
 // O(n), no faster way if list unsorted.
 func findSmallestNumber(input []int) int {
@@ -12,4 +15,24 @@ func findSmallestNumber(input []int) int {
 		}
 	}
 	return smallest
+}
+
+func findNMins(input []int, minN int) []int {
+	mins := make([]int, minN)
+	//initialize array with max numbers
+	for i := 0; i < minN; i++ {
+		mins[i] = int(math.MaxInt64)
+	}
+
+	for _, v := range input {
+		mins = sortSmallest(mins, minN, v)
+	}
+
+	return mins
+}
+
+func sortSmallest(input []int, N, newNum int) []int {
+	input = append(input, newNum)
+	sort.Slice(input, func(i, j int) bool { return input[i] < input[j] })
+	return input[:N]
 }
--- questions/norderedstatistic/stats_test.go
+++ questions/norderedstatistic/stats_test.go
@@ -39,3 +39,41 @@ func TestSmallest(t *testing.T) {
 		})
 	}
 }
+
+func TestSortSmallestN(t *testing.T) {
+	tests := []struct {
+		input    []int
+		N        int
+		smallest []int
+	}{
+		{
+			input:    []int{2, 1, 6, 3, 9, 7, 5},
+			N:        1,
+			smallest: []int{1},
+		},
+		{
+			input:    []int{2, 1, 6, 3, 9, 7, 5},
+			N:        2,
+			smallest: []int{1, 2},
+		},
+		{
+			input:    []int{2, -1, 6, 3, 9, 7, 5},
+			N:        3,
+			smallest: []int{-1, 2, 3},
+		},
+		{
+			input:    []int{2, -1, -1, 3, 9, 7, 5},
+			N:        3,
+			smallest: []int{-1, -1, 2},
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("testSmallest-%d", i), func(t *testing.T) {
+			mins := sortSmallest(test.input, test.N, int(math.MaxInt64))
+			if !reflect.DeepEqual(mins, test.smallest) {
+				t.Errorf("expected smallest %v != calculated: %v", test.smallest, mins)
+			}
+		})
+	}
+}
