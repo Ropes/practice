6a63b2efc0025c8fadcc268a26cfbb186610fa0b
Joshua <joshroppo@gmail.com>
2020-02-05 14:43:13 -0800

    Find the Nth smallest number in place with insertion sort algorithm and trimming.

--- questions/norderedstatistic/stats.go
+++ questions/norderedstatistic/stats.go
@@ -2,6 +2,7 @@
 package norderedstatistic
 
 import (
+	"fmt"
 	"math"
 	"sort"
 )
@@ -29,6 +30,44 @@ func findNMins(input []int, minN int) []int {
 	}
 
 	return mins
+}
+
+// Find Nth smallest number with traversal and splitting efficiency.
+// Similar to insertion sort, traverse first number to position where it is smaller than numbers to right.
+// Reduce input to have the first N values sorted in the list.
+// []int{10,2,5,3,6,11,15,-1,-2} wE: 9
+// []int{2,5,3,6,10,11,15,-1,-2}
+// []int{2,5,3,6,10,11,-1,15,-2}
+// []int{2,5,3,6,10,11,-1,-2,15} wE: 8
+// []int{2,3,5,6,10,11,-1,-2,15} wE: 8
+// []int{2,3,5,6,10,-1,11,-2,15} wE: 8
+// []int{2,3,5,6,10,-1,-2,11,15} wE: 7
+// []int{2,3,5,6,-1,10,-2,11,15} wE: 7
+// []int{2,3,5,6,-1,-2,10,11,15} wE: 6
+func findNthSmallest(input []int, n int) int {
+	windowStart, windowEnd := 0, len(input)-1
+	for (windowEnd - windowStart) > n {
+		swpPt := 0
+		fmt.Printf("input: %v\n", input)
+		for i := windowStart + 1; i <= windowEnd; i++ {
+			if input[i-1] > input[i] { // 10 > [i]
+				input[i], input[i-1] = input[i-1], input[i] //swap the values
+				swpPt = i                                   // move to next comparison value
+			}
+		}
+		if swpPt < windowEnd {
+			windowEnd = swpPt
+		}
+	}
+
+	// Pick the Nth smallest(largest value from size N)
+	smallest := input[0]
+	for i := 0; i < windowEnd; i++ {
+		if smallest < input[i] {
+			smallest = input[i]
+		}
+	}
+	return smallest
 }
 
 func sortSmallest(input []int, N, newNum int) []int {
--- questions/norderedstatistic/stats_test.go
+++ questions/norderedstatistic/stats_test.go
@@ -109,6 +109,11 @@ func TestSmallestN(t *testing.T) {
 			N:        2,
 			smallest: []int{-1, -1},
 		},
+		{
+			input:    []int{2, -1, -1, 3, 9, 7, 5},
+			N:        1,
+			smallest: []int{-1},
+		},
 	}
 
 	for i, test := range tests {
@@ -119,4 +124,33 @@ func TestSmallestN(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestInPlaceSmallestN(t *testing.T) {
+	tests := []struct {
+		input []int
+		N     int
+		exp   int
+	}{
+		{
+			input: []int{10, 2, 5, 3, 6, 11, 15, -1, -2},
+			N:     3,
+			exp:   2,
+		},
+		{
+			input: []int{10, 2, 5, 3, 6, 11, 15, -1, -2},
+			N:     5,
+			exp:   5,
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test-in-place-%d", i), func(t *testing.T) {
+			v := findNthSmallest(test.input, test.N)
+			if v != test.exp {
+				t.Errorf("wrong value returned: %d != %d", v, test.exp)
+			}
+		})
+	}
+
 }
